ac98f106c7412325c1477da5f580dec7564334dd
Varun Kumar Rachamalla <varunkumarrachamalla@live.com>
2023-02-23 11:03:53 +0530

    updated static files url

--- azureproject/settings.py
+++ azureproject/settings.py
@@ -145,10 +145,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+     os.path.join(BASE_DIR, 'static')
+     ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+ 
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
