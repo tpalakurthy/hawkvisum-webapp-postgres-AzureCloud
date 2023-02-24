ff36e7013b2a769a4c9e2c2c365b9aeaaaf86ac7
Varun Kumar Rachamalla <varunkumarrachamalla@live.com>
2023-02-24 14:54:58 +0530

    Updated csrf origin

--- azureproject/production.py
+++ azureproject/production.py
@@ -6,7 +6,7 @@ from .settings import BASE_DIR
 # Configure the domain name using the environment variable
 # that Azure automatically creates for us.
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
-CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+# CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 DEBUG = False
 
 # WhiteNoise configuration
