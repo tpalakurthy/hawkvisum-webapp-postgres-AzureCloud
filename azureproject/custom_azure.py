a893b4190fb21839768d81a9558390cbcd016ec0
Varun Kumar Rachamalla <varunkumarrachamalla@Varuns-MacBook-Air.local>
2023-02-26 08:51:44 +0530

    Implemented Azure blob storage

---
+++ azureproject/custom_azure.py
@@ -0,0 +1,13 @@
+from storages.backends.azure_storage import AzureStorage
+
+class AzureMediaStorage(AzureStorage):
+    account_name = "hawkvisumfilestorage"
+    account_key = 'VCVdWsO00YMX/5krpUj1r8cpBIaa+VMlymtg2iY88zNjGjOe8F/q99gOV1Eqfeu6+9r215u00gcd+AStp7mF1A=='
+    azure_container = 'media'
+    expiration_secs = None
+
+class AzureStaticStorage(AzureStorage):
+    account_name = '"hawkvisumfilestorage"
+    account_key = 'VCVdWsO00YMX/5krpUj1r8cpBIaa+VMlymtg2iY88zNjGjOe8F/q99gOV1Eqfeu6+9r215u00gcd+AStp7mF1A=='
+    azure_container = 'static'
+    expiration_secs = None
